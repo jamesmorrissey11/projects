36c9046f72ebd3d7fa8940fd35c9a5246b7a8451
jamesmorrissey11 <142894427+jamesmorrissey11@users.noreply.github.com>
2023-08-31 04:03:10 +0000

    embed docs and store in vectordb

---
+++ code_understanding/ingest.py
@@ -0,0 +1,56 @@
+import argparse
+import json
+import os
+from datetime import datetime
+from pathlib import Path
+from typing import Dict, List
+
+from langchain.docstore.document import Document
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import DeepLake
+from utility import get_logger, log_model_config
+
+import os
+
+os.environ["OPENAI_API_KEY"] = "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"
+
+logger = get_logger()
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--dataset", type=str, required=True, help="location of json data"
+    )
+    parser.add_argument(
+        "--model_dir", type=str, required=True, help="where to store vector db"
+    )
+    args = parser.parse_args()
+
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    args.model_dir = os.path.join(args.model_dir, timestamp)
+
+    return args
+
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    logger.info(f"Loading data from {args.dataset}")
+    data: List[Dict] = json.loads(Path(args.dataset).read_text())
+    docs = [
+        Document(page_content=d["page_content"], metadata=d["metadata"]) for d in data
+    ]
+
+    logger.info(f"Generating vector db to be stored at {args.model_dir}")
+    db = DeepLake.from_documents(
+        docs,
+        OpenAIEmbeddings(disallowed_special=()),
+        dataset_path=args.model_dir,
+        overwrite=True,
+    )
+    log_model_config(
+        logger=logger,
+        args=args,
+        log_dir="/Users/jamesmorrissey/Tutorials/portfolio/code-understanding/model",
+    )
